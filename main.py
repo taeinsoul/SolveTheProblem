c26854f8f38ec6116e402a3c67bd25d1a2ddbc59
taein.seol <taeinsoul@gmail.com>
2022-03-04 08:09:34 +0000

    update web scrape

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 
 #import quiz
 #quiz.quiz()
-import web_scrape
-web_scrape.scrape_func()
+
+from web_scrape import scraping_mossblanc_lists
+from web_scrape import scraping_mossblanc_list_one_by_one_save_csv
+mainUrl = 'https://smartstore.naver.com/mossblanc/'
+index_list = scraping_mossblanc_lists(mainUrl)
+scraping_mossblanc_list_one_by_one_save_csv(index_list)
--- web_scrape.py
+++ web_scrape.py
@@ -1,21 +1,44 @@
 from bs4 import BeautifulSoup
 import requests
-def scrape_func():
-  default_url = 'https://smartstore.naver.com'
-  url = 'https://smartstore.naver.com/mossblanc/'
+import csv
+
+default_url = 'https://smartstore.naver.com'
+
+def scrape_func(url):
   req = requests.get(url)
   soup = BeautifulSoup(req.content,'html.parser')
-  print(soup.title)
   lists = soup.find_all('a')
   pages = []
   for ix in range(0, len(lists)):
     list = lists[ix].get('href')
-    pages.append(list)
-    print(f"list {ix} :{list}")
+    if list.find("products") != -1:
+      pages.append(list)
+      
   subpage = []
   for p in range(0,len(pages)):
     if "mossblanc/products" in pages[p]:
         pullurl = default_url + pages[p]
-        print(f"subpage {p}: {pullurl}")    
+        #print(f"list {p}: {pullurl}")    
         subpage.append(pullurl)
-      
+  return subpage
+
+def scraping_mossblanc_lists(WebUrl):
+  request = requests.get(WebUrl)
+  soupListData = BeautifulSoup(request.content,'html.parser')
+  contentsList = soupListData.find_all('a','_3HQCww4jR6')
+  index_lists = []
+  for list in contentsList:
+    print(list.get_text())
+    FullUrl = default_url+list.get('href')
+    index_lists.append([list.get_text(), FullUrl])
+  return index_lists
+
+def scraping_mossblanc_list_one_by_one_save_csv(lists):
+  f = open('output.csv', 'w', encoding='utf-8', newline='')
+  wr = csv.writer(f)
+  
+  for item in lists:
+    wr.writerow([item[0]])
+    for url in scrape_func(item[1]):
+      wr.writerow([url])
+    
